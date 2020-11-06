64359fa1d70e29a37a7f95dc39958559efed8aea
Parrot user <user@localhost.localdomain>
2020-11-06 07:45:45 +0000

    first commit

---
+++ Andromeda.py
@@ -0,0 +1,21 @@
+import socket
+import os
+import subprocess
+import time
+
+print("   ___              __                              __     ")
+time.sleep(0.2)
+print("   /   |  ____  ____/ /________  ____ ___  ___  ____/ /___ _")
+time.sleep(0.2)
+print("  / /| | / __ \/ __  / ___/ __ \/ __ `__ \/ _ \/ __  / __ `/")
+time.sleep(0.2)
+print(" / ___ |/ / / / /_/ / /  / /_/ / / / / / /  __/ /_/ / /_/ / ")
+time.sleep(0.2)
+print("/_/  |_/_/ /_/\__,_/_/   \____/_/ /_/ /_/\___/\__,_/\__,_/  ")
+time.sleep(0.2)
+print("                                                            ")
+time.sleep(0.2)
+print("\n\n")
+print("Type help to view commands")
+input1 = input("> ")
+print(input1)
