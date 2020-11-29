95dbea3a8c57f692a13ae47d5a156b6b951b6421
fluffy <thefluffysnowmanyt@gmail.com>
2020-11-29 21:17:15 +0000

    deleted test file

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
 
-try: 
-    from googlesearch import search 
-except ImportError:  
-    print("No module named 'google' found") 
-  
-# to search 
-query = "A computer science portal"
-  
-for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
-    print(j) 
