663b7ba2f19c45dc7a6c3e3bd4aa14142185b0c7
fluuffy <thefluffysnowmanyt@gmail.com>
2021-02-06 12:51:09 +0530

    updating help list part 1

--- Andromeda.py
+++ Andromeda.py
@@ -71,7 +71,7 @@ while 1 < 2:
     if (input1 == "help"):
 
 
-        print(Fore.MAGENTA+"Find the docs at https://github.com/FluffySnowman/projectAndromeda/blob/master/docs.md\n\n"+Fore.RED+Fore.GREEN+"All Commands: shell, wifiaudit, help, googlescrape, twitterscrape, yt-download, cls, exit"+Fore.RED)
+        print(Fore.MAGENTA+"Find the docs at https://github.com/FluffySnowman/projectAndromeda/blob/master/docs.md\n\n"+Fore.RED+Fore.GREEN+"help: shows this list\nshell: opens a shell with the default shell of your operating system(i.e:- bash, cmd)\nwifiaudit:, googlescrape, twitterscrape, yt-download, cls, exit"+Fore.RED)
 
 
     #####################################
